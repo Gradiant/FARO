a624bd1c0ad45cb40e8d64beb1927169566fe1d0
Hector Cerezo <hcerezo@gradiant.org>
2019-10-25 12:37:28 +0200

    Deleting prob_currency from the header in dumps

--- faro_spider.sh
+++ faro_spider.sh
@@ -12,7 +12,7 @@ fi
 
 CPU_PARALLEL_USAGE="50%"
 
-echo "filepath,score,person_position_organization,monetary_quantity,signature,personal_email,mobile_phone_number,financial_data,document_id,custom_words,prob_currency,content-type" > output/scan.$SUFFIX.csv
+echo "filepath,score,person_position_organization,monetary_quantity,signature,personal_email,mobile_phone_number,financial_data,document_id,custom_words,content-type" > output/scan.$SUFFIX.csv
 find $1 -type f \( -iname "*.doc" -o -iname "*.docx" -o -iname "*.pptx" -o -iname "*.ppt" -o -iname "*.xls*" -o -iname "*.pdf" -o -iname "*.odt" -o -iname "*.ods" -o -iname "*.odp" -o -iname "*.txt" -o -iname "*.rtf" \)  | parallel -P $CPU_PARALLEL_USAGE python faro_detection.py -i {} --output_entity_file output/scan.$SUFFIX.entity --dump >> output/scan.$SUFFIX.csv
 
 sed -i.bak 's/$/,/' output/scan.$SUFFIX.entity
