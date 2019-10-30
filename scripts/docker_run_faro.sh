4800a0260dc898bcca8c91e340b2c706763b65a2
Juan Elosua <juan.elosua@gmail.com>
2019-10-30 16:51:41 +0100

    fix double quotes issue on UNIX architectures, add cleanup for docker execution

--- scripts/docker_run_faro.sh
+++ scripts/docker_run_faro.sh
@@ -2,4 +2,8 @@
 echo "Analyzing documents..."
 INPUT_PATH=$(cd $1; pwd)
 mkdir -p output
-docker run -v $INPUT_PATH:/opt/faro/input -v $PWD/output:/opt/faro/output faro input \"$INPUT_PATH\"
+# Generate random id to be able to cleanup after spider execution
+RANDOM_NAME=$(dd bs=1 count=16 if=/dev/urandom 2> /dev/null | xxd -p)
+docker run --name "faro_"$RANDOM_NAME -v "$INPUT_PATH":/opt/faro/input -v "$PWD"/output:/opt/faro/output faro input "$INPUT_PATH"
+# Cleanup
+docker rm "faro_"$RANDOM_NAME 2> /dev/null
