79d32e81a8a19561bb3540f3a4251a74032a6769
Hector Cerezo <hcerezo@gradiant.org>
2019-10-30 14:06:26 +0100

    Starting tika server with nohup

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 PREFFIX=$(echo $2 | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')
 SUFFIX=$(date "+%Y.%m.%d-%H.%M.%S")
 
-java -jar /tmp/tika-server.jar -h 0.0.0.0 &
+nohup java -jar /tmp/tika-server.jar -h 0.0.0.0 &
 
 ./faro_spider.sh $1 $SUFFIX
 
