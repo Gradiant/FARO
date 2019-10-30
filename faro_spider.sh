bc47d0f60bd3598ad431b4b138bb602f2757352f
Juan Elosua <juan.elosua@gmail.com>
2019-10-30 18:58:35 +0100

    add comments to the spider and checks to run locally

--- faro_spider.sh
+++ faro_spider.sh
@@ -2,13 +2,21 @@
 mkdir -p output
 
 # Run tika server externally
+
+# Check if we have the tika server jar, if not download it
 TIKA_SERVER_JAR=/tmp/tika-server.jar
 if [ ! -f $TIKA_SERVER_JAR ]; then
    	wget -O $TIKA_SERVER_JAR https://repo1.maven.org/maven2/org/apache/tika/tika-server/1.21/tika-server-1.21.jar
 fi
-java -jar $TIKA_SERVER_JAR -h 0.0.0.0 &
-# Allow tika some uptime to finish initialization
-sleep 5
+
+# Check if tika is running locally already
+nc -z 0.0.0.0 9998 2>/dev/null
+if [ $? -ne 0 ]
+then
+	java -jar $TIKA_SERVER_JAR -h 0.0.0.0 &
+	# Allow tika some uptime to finish initialization
+	sleep 5
+fi
 
 if [ -z "$2" ]
 then
