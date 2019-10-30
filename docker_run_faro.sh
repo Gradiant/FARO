a161bc31b8ccdadbfd957dac9ba390cbb1511d7d
Juan Elosua <juan.elosua@gmail.com>
2019-10-30 21:51:51 +0100

    fix spaces issue on input path and ignore docker rm message

--- docker_run_faro.sh
+++ docker_run_faro.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "Analyzing documents..."
-INPUT_PATH=$(cd $1; pwd)
+INPUT_PATH=$(cd "$1"; pwd)
 mkdir -p output
 # Generate random id to be able to cleanup after spider execution
 RANDOM_NAME=$(dd bs=1 count=16 if=/dev/urandom 2> /dev/null | xxd -p)
 docker run --name "faro_"$RANDOM_NAME -v "$INPUT_PATH":/opt/faro/input -v "$PWD"/output:/opt/faro/output faro input "$INPUT_PATH"
 # Cleanup
-docker rm "faro_"$RANDOM_NAME 2> /dev/null
+docker rm "faro_"$RANDOM_NAME >/dev/null
