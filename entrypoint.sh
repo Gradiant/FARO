06f47161dda9183323fe942d2d998847b2214200
Juan Elosua <juan.elosua@gmail.com>
2019-10-31 13:34:38 +0100

    fix typo and add clarity to windows vs unix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-PREFFIX=$(echo $2 | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')
+PREFIX=$(echo $2 | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')
 SUFFIX=$(date "+%Y.%m.%d-%H.%M.%S")
 
 ./faro_spider.sh $1 $SUFFIX
 
-if [ -z "$3" ]
+if [[ -z "$3" || "$3" != "win" ]]
 then
-   sed -i.bak "s|input|$PREFFIX|" output/scan.$SUFFIX.csv
-   sed -i.bak "s|input|$PREFFIX|" output/scan.$SUFFIX.entity
+	# UNIX
+   	sed -i.bak "s|input|$PREFIX|" output/scan.$SUFFIX.csv
+   	sed -i.bak "s|input|$PREFIX|" output/scan.$SUFFIX.entity
 else
-   sed -i.bak "s|input|$PREFFIX| ; s|/|\\\\|g ; s|,application\\\\|,application\/| ; s|,text\\\\|,text\/|" output/scan.$SUFFIX.csv
-   sed -i.bak "s|input|$PREFFIX| ; s|/|\\\\|g ; s|,application\\\\|,application\/|" output/scan.$SUFFIX.entity
+	# Windows
+   	sed -i.bak "s|input|$PREFIX| ; s|/|\\\\|g ; s|,application\\\\|,application\/| ; s|,text\\\\|,text\/|" output/scan.$SUFFIX.csv
+   	sed -i.bak "s|input|$PREFIX| ; s|/|\\\\|g ; s|,application\\\\|,application\/|" output/scan.$SUFFIX.entity
 fi
 
 rm output/scan.$SUFFIX.csv.bak
