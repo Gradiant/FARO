97a6455d3ce59b9d9ef200475962a52514b5b6da
Rafael Pedro Martínez Álvarez <rmartinez@gradiant.org>
2019-11-04 19:59:42 +0100

    Some docker tunings for Windows platforms

--- docker_build_faro.bat
+++ docker_build_faro.bat
@@ -1,2 +1,12 @@
-@echo off
-docker build -t faro .
+@ECHO off
+git lfs pull
+
+REM Check exit status of git lfs pull
+IF %ERRORLEVEL% EQU 0 (
+   ECHO Building gradiant/faro docker image.
+   docker build -t gradiant/faro:1.0.1 .
+   EXIT /B 0
+) ELSE (
+   ECHO Failure: You should have git lfs installed in your host machine. Check README file 1>&2
+   EXIT /B 1
+)
