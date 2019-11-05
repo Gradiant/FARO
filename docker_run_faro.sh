478c8573f2aba4e649e9165ab5fae2ba10cc5323
Hector Cerezo <hcerezo@gradiant.org>
2019-11-05 16:17:22 +0100

    Update Dockerfile to remove java8 depedency and use the openjdk11 Fix pythondateutil dependency issue

--- docker_run_faro.sh
+++ docker_run_faro.sh
@@ -2,4 +2,4 @@
 echo "Analyzing documents..."
 INPUT_PATH=$(cd "$1"; pwd)
 mkdir -p output
-docker run --rm --name "gradiant/faro:1.0.1" -v "$INPUT_PATH":/opt/faro/input -v "$PWD"/output:/opt/faro/output gradiant/faro:1.0.1 input "$INPUT_PATH"
+docker run --rm -v "$INPUT_PATH":/opt/faro/input -v "$PWD"/output:/opt/faro/output gradiant/faro:1.0.1 input "$INPUT_PATH"
