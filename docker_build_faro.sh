7cb40c50c55785da36efec9ff5e0657891a52303
Hector Cerezo <hcerezo@gradiant.org>
2019-11-05 16:17:22 +0100

    Update Dockerfile to remove java8 depedency and use the openjdk11 Fix pythondateutil dependency issue

--- docker_build_faro.sh
+++ docker_build_faro.sh
@@ -5,6 +5,7 @@ docker build -t gradiant/faro:1.0.1 .
 
 git lfs pull
 if [ $? -ne 0 ]
+then
   echo "Warning: You should either have git lfs installed on your host machine or manually download the models to get a successful build. Check README for details." >&2
   exit 1
 fi
