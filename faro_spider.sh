c448054583b2f9e5046cd7767748ca0997629adb
Juan Elosua <juan.elosua@gmail.com>
2019-11-27 16:48:15 +0100

    let tika handle supported formats but exclude hidden files

--- faro_spider.sh
+++ faro_spider.sh
@@ -38,4 +38,4 @@ CPU_PARALLEL_USAGE="50%"
 
 echo "filepath,score,person_position_organization,monetary_quantity,signature,personal_email,mobile_phone_number,financial_data,document_id,custom_words,content-type" > output/scan.$SUFFIX.csv
 # Run faro over a recursive list of appropriate filetypes
-find "$1" -type f \( -iname "*.doc" -o -iname "*.docx" -o -iname "*.pptx" -o -iname "*.ppt" -o -iname "*.xls*" -o -iname "*.pdf" -o -iname "*.odt" -o -iname "*.ods" -o -iname "*.odp" -o -iname "*.txt" -o -iname "*.rtf" \)  | parallel -P $CPU_PARALLEL_USAGE python faro_detection.py -i {} --output_entity_file output/scan.$SUFFIX.entity --dump >> output/scan.$SUFFIX.csv
+find "$1" -type f \( ! -regex '.*/\..*' \) | parallel -P $CPU_PARALLEL_USAGE python faro_detection.py -i {} --output_entity_file output/scan.$SUFFIX.entity --dump >> output/scan.$SUFFIX.csv
