7e5974268210503f8e117e1d7cf6bf7a8168e229
Juan Elosua <juan.elosua@gmail.com>
2019-11-27 16:48:15 +0100

    let tika handle supported formats but exclude hidden files

--- faro_spider.sh
+++ faro_spider.sh
@@ -7,7 +7,7 @@ mkdir -p output
 TIKA_SERVER_JAR=/tmp/tika-server.jar
 
 if [ ! -f $TIKA_SERVER_JAR ]; then
-   	wget -O $TIKA_SERVER_JAR https://repo1.maven.org/maven2/org/apache/tika/tika-server/1.21/tika-server-1.21.jar
+   	wget -O $TIKA_SERVER_JAR https://repo1.maven.org/maven2/org/apache/tika/tika-server/1.22/tika-server-1.22.jar
 fi
 
 # Get rid of tika server initialization warnings
@@ -38,4 +38,4 @@ CPU_PARALLEL_USAGE="50%"
 
 echo "filepath,score,person_position_organization,monetary_quantity,signature,personal_email,mobile_phone_number,financial_data,document_id,custom_words,content-type" > output/scan.$SUFFIX.csv
 # Run faro over a recursive list of appropriate filetypes
-find "$1" -type f \( -iname "*.doc" -o -iname "*.docx" -o -iname "*.pptx" -o -iname "*.ppt" -o -iname "*.xls*" -o -iname "*.pdf" -o -iname "*.odt" -o -iname "*.ods" -o -iname "*.odp" -o -iname "*.txt" -o -iname "*.rtf" \)  | parallel -P $CPU_PARALLEL_USAGE python faro_detection.py -i {} --output_entity_file output/scan.$SUFFIX.entity --dump >> output/scan.$SUFFIX.csv
+find "$1" -type f \( ! -regex '.*/\..*' \) | parallel -P $CPU_PARALLEL_USAGE python faro_detection.py -i {} --output_entity_file output/scan.$SUFFIX.entity --dump >> output/scan.$SUFFIX.csv
