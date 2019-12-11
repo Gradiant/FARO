2854cd95ffd2739ffaa38632ee2addebe7d31dd3
Juan Elosua <juan.elosua@gmail.com>
2019-12-11 15:34:12 +0100

    add ocr disable mechanism and update version

--- docker_run_faro.bat
+++ docker_run_faro.bat
@@ -4,4 +4,8 @@ PUSHD %1
 SET INPUT_PATH=%CD%
 POPD
 IF NOT EXIST output MKDIR output
-docker run --rm --name "gradiant-faro" -v "%INPUT_PATH%":/opt/faro/input -v "%cd%"\output:/opt/faro/output gradiant/faro:1.0.1 input "%INPUT_PATH%" win
+IF %2 == "--no-ocr" (
+	docker run -e FARO_DISABLE_OCR="YES" --rm --name "gradiant-faro" -v "%INPUT_PATH%":/opt/faro/input -v "%cd%"\output:/opt/faro/output gradiant/faro:1.1.0 input "%INPUT_PATH%" win
+) ELSE (
+	docker run --rm --name "gradiant-faro" -v "%INPUT_PATH%":/opt/faro/input -v "%cd%"\output:/opt/faro/output gradiant/faro:1.1.0 input "%INPUT_PATH%" win
+)
--- docker_run_faro.sh
+++ docker_run_faro.sh
@@ -2,4 +2,11 @@
 echo "Analyzing documents..."
 INPUT_PATH=$(cd "$1"; pwd)
 mkdir -p output
-docker run --rm -v "$INPUT_PATH":/opt/faro/input -v "$PWD"/output:/opt/faro/output gradiant/faro:1.0.1 input "$INPUT_PATH"
+if [ "$2" == "--no-ocr" ]
+then
+	echo "found --no-ocr argument"
+	docker run -e FARO_DISABLE_OCR="YES" --rm -v "$INPUT_PATH":/opt/faro/input -v "$PWD"/output:/opt/faro/output gradiant/faro:1.1.0 input "$INPUT_PATH"
+else
+	docker run --rm -v "$INPUT_PATH":/opt/faro/input -v "$PWD"/output:/opt/faro/output gradiant/faro:1.1.0 input "$INPUT_PATH"
+fi
+
