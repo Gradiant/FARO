8eea8d96a933e639305485dad880e30f5ecf9f80
Juan Elosua <juan.elosua@gmail.com>
2019-12-12 18:05:57 +0100

    add envfile for windows

--- docker_run_faro.bat
+++ docker_run_faro.bat
@@ -5,7 +5,7 @@ SET INPUT_PATH=%CD%
 POPD
 IF NOT EXIST output MKDIR output
 IF "%2" == "--no-ocr" (
-	docker run -e FARO_DISABLE_OCR="YES" --rm --name "gradiant-faro" -v "%INPUT_PATH%":/opt/faro/input -v "%cd%"\output:/opt/faro/output gradiant/faro:1.1.0 input "%INPUT_PATH%" win
+	docker run --env-file "%2" --rm --name "gradiant-faro" -v "%INPUT_PATH%":/opt/faro/input -v "%cd%"\output:/opt/faro/output gradiant/faro:1.1.0 input "%INPUT_PATH%" win
 ) ELSE (
 	docker run --rm --name "gradiant-faro" -v "%INPUT_PATH%":/opt/faro/input -v "%cd%"\output:/opt/faro/output gradiant/faro:1.1.0 input "%INPUT_PATH%" win
 )
