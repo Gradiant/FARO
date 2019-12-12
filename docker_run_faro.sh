bec68183fcd0812ffdd9e378420c25da33a25806
Juan Elosua <juan.elosua@gmail.com>
2019-12-12 17:59:32 +0100

    move OCR customization to envvars, use an envfile for docker and update docs

--- docker_run_faro.sh
+++ docker_run_faro.sh
@@ -2,11 +2,11 @@
 echo "Analyzing documents..."
 INPUT_PATH=$(cd "$1"; pwd)
 mkdir -p output
-if [ "$2" == "--no-ocr" ]
+
+if [ -z "$2" ]
 then
-	echo "found --no-ocr argument"
-	docker run -e FARO_DISABLE_OCR="YES" --rm -v "$INPUT_PATH":/opt/faro/input -v "$PWD"/output:/opt/faro/output gradiant/faro:1.1.0 input "$INPUT_PATH"
-else
 	docker run --rm -v "$INPUT_PATH":/opt/faro/input -v "$PWD"/output:/opt/faro/output gradiant/faro:1.1.0 input "$INPUT_PATH"
+else
+   	docker run --env-file "$2" --rm -v "$INPUT_PATH":/opt/faro/input -v "$PWD"/output:/opt/faro/output gradiant/faro:1.1.0 input "$INPUT_PATH"
 fi
 
