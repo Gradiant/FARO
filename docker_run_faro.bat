e7fb59fb9125297c14ce65b92a567978091ae061
Rafael Martínez <56405683+tantalizer@users.noreply.github.com>
2019-12-12 20:05:10 +0100

    correction on env-file IF statement

--- docker_run_faro.bat
+++ docker_run_faro.bat
@@ -4,8 +4,8 @@ PUSHD %1
 SET INPUT_PATH=%CD%
 POPD
 IF NOT EXIST output MKDIR output
-IF "%2" == "--no-ocr" (
-	docker run --env-file "%2" --rm --name "gradiant-faro" -v "%INPUT_PATH%":/opt/faro/input -v "%cd%"\output:/opt/faro/output gradiant/faro:1.1.0 input "%INPUT_PATH%" win
-) ELSE (
+IF [%2]==[] (
 	docker run --rm --name "gradiant-faro" -v "%INPUT_PATH%":/opt/faro/input -v "%cd%"\output:/opt/faro/output gradiant/faro:1.1.0 input "%INPUT_PATH%" win
+) ELSE (
+	docker run --env-file "%2" --rm --name "gradiant-faro" -v "%INPUT_PATH%":/opt/faro/input -v "%cd%"\output:/opt/faro/output gradiant/faro:1.1.0 input "%INPUT_PATH%" win
 )
