1fd1b734cfd2e153ac4eddf340c3c7b137c9aa68
Hector Cerezo <hcerezo@gradiant.org>
2019-12-19 17:01:02 +0100

    Retagging docker container in scripts

--- docker_run_faro.sh
+++ docker_run_faro.sh
@@ -5,8 +5,8 @@ mkdir -p output
 
 if [ -z "$2" ]
 then
-	docker run --rm -v "$INPUT_PATH":/opt/faro/input -v "$PWD"/output:/opt/faro/output gradiant/faro:1.1.1 input "$INPUT_PATH"
+	docker run --rm -v "$INPUT_PATH":/opt/faro/input -v "$PWD"/output:/opt/faro/output gradiant/faro:latest input "$INPUT_PATH"
 else
-   	docker run --env-file "$2" --rm -v "$INPUT_PATH":/opt/faro/input -v "$PWD"/output:/opt/faro/output gradiant/faro:1.1.1 input "$INPUT_PATH"
+   	docker run --env-file "$2" --rm -v "$INPUT_PATH":/opt/faro/input -v "$PWD"/output:/opt/faro/output gradiant/faro:latest input "$INPUT_PATH"
 fi
 
